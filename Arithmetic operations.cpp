719b0b0604ce7418a6d937d397d49a85e889bb46
Kaveri1807 <kaveri.rajguru24@sanjivani.edu.in>
2024-09-28 10:10:09 +0530

    Create Arithmetic operations.cpp
    This C++ program demonstrates basic arithmetic operations.
    It initializes two integers, a (8) and b (3), and performs five operations:
    Addition, subtraction, multiplication, division, and modulus (remainder).
    The program displays the results of each operation.
    The operations are executed using standard operators (+, -, *, /, %).
    The output showcases the calculated values for each arithmetic operation.

---
+++ Arithmetic operations.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 8, b = 3;
+    // addition operator
+    cout << "a + b= " << (a + b) << endl;
+    // substraction operator
+    cout << "a - b " << (a - b) << endl;
+    // Multiplication operator
+    cout << "a * b " << (a*b) << endl;
+    // Division  operator
+    cout << "a / b " << (a / b) << endl;
+    // Modulo  operator
+    cout << "a % b= " << (a % b) << endl;
+    return 0;
+}
