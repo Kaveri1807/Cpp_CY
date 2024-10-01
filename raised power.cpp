91c66401c65f22b5340bb16f8161e864cb376632
Kaveri1807 <kaveri.rajguru24@sanjivani.edu.in>
2024-10-01 20:49:39 +0530

    Create raised power.cpp
    Calculates the result of raising a number to a power using the pow() function.Demonstrates the use of the cmath library for mathematical operations.Takes two arguments (base and exponent) and returns the calculated value.Displays the result of 2 raised to the power of 3.Illustrates basic mathematical operations in C++.

---
+++ raised power.cpp
@@ -0,0 +1,10 @@
+#include <cmath>
+#include <iostream>
+using namespace std; 
+
+int main() {
+    double num = (2,3);
+cout << "2 raised to the power 3 is: " << pow(2,3) << endl; 
+    
+    return 0;
+}
